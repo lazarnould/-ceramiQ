e46fad9547eb4d326414ca993b5d9a3d23a1d97f
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-06-08 17:29:20 +0200

    add delete btn on product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = "You've add #{@product.name} on the database, add some specifications to put it on sale"
-      redirect_to new_product_specification(@product)
+      redirect_to new_product_specification_path(@product.id)
     else
       flash[:notice] = "A problem occured, try again"
       render :new
