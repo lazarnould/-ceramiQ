16a2048e82ecc5de8d153af0cd346b325298a49a
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-06-08 15:59:50 +0200

    product_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,45 @@
 class ProductsController < ApplicationController
+
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @products = Product.all
   end
 
   def show
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_path(@product)
   end
 
   def edit
   end
 
   def update
+    @product.update(product_params)
+    redirect_to product_path(@product)
   end
 
   def destroy
+    @product
   end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :gender, :price, :category, :description, :season)
+  end
+
 end
+
