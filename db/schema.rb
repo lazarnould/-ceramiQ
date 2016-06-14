e67165a1fbaf75e6123262940b8fac95a8c70a27
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-06-14 11:38:10 +0200

    monetize gem & stripe beginning

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613110749) do
+ActiveRecord::Schema.define(version: 20160614091926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,13 +61,13 @@ ActiveRecord::Schema.define(version: 20160613110749) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.integer  "price"
     t.string   "gender"
     t.string   "category"
     t.string   "description"
     t.string   "season"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "price_cents", default: 0, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
