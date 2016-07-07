5c2ebbc653aeed7f7ea9651360010f163cc9dc36
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-07-07 21:43:46 +0200

    categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "The #{@category.name} category has been destroyed"
+      redirect_to new_category_product(@category)
+    else
+      flash[:alert] = "A problem occured, please try again"
+      render :new
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
