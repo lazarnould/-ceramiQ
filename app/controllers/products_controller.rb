aa874de131d19c0f2d0cb66bcb36a9daa8aefe2e
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-07-11 12:27:23 +0200

    types in product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :gender, :price_cents, :category, :description, :season, :photo, :photo_cache)
+    params.require(:product).permit(:name, :gender, :price_cents, :category, :description, :season, :photo, :photo_cache, :type)
   end
 
   def specification_params
